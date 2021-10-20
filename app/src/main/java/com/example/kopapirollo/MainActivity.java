b55f08c485f0ff5a2b70b57d2367c7f53c9bf099
Kristof <korcsmaros.kristof.gyorgy@petrik.hu>
2021-10-20 17:40:46 +0200

    Gombok kezelése

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -3,21 +3,111 @@ package com.example.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView1, imageView2;
     private TextView textView1;
     private Button btn1, btn2, btn3;
+    private int pontEmber, pontComputer, dontetlen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mutat(1);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mutat(2);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mutat(3);
+            }
+        });
+    }
+
+    private void kepBeallitas(int valasztas, ImageView imageView){
+        switch (valasztas){
+            case 1:
+                imageView.setImageResource(R.drawable.rock);
+                break;
+            case 2:
+                imageView.setImageResource(R.drawable.paper);
+                break;
+            case 3:
+                imageView.setImageResource(R.drawable.scissors);
+                break;
+        }
+    }
+
+    private void gyozelem(){
+        Toast.makeText(this, "Győzelem", Toast.LENGTH_SHORT).show();
+        pontEmber++;
+        textView1.setText("Eredmény: Ember: " + pontEmber + " Computer: " + pontComputer);
+    }
+
+    private void vereseg(){
+        Toast.makeText(this, "Vereség", Toast.LENGTH_SHORT).show();
+        pontComputer++;
+        textView1.setText("Eredmény: Ember: " + pontEmber + " Computer: " + pontComputer);
+    }
+
+    private void dontetlen(){
+        Toast.makeText(this, "Döntetlen", Toast.LENGTH_SHORT).show();
+        dontetlen++;
+    }
+
+    private void mutat(int valasztas){
+        kepBeallitas(valasztas, imageView1);
+        int gepValasztas = (int) (Math.random() * 3) + 1;
+        kepBeallitas(gepValasztas, imageView2);
+
+        if (valasztas == gepValasztas){
+            dontetlen();
+        }
+        else if (valasztas == 1){
+            if (gepValasztas == 2){
+                vereseg();
+            }
+            else{
+                gyozelem();
+            }
+        }
+        else if (valasztas == 2){
+            if (gepValasztas == 1){
+                gyozelem();
+            }
+            else{
+                vereseg();
+            }
+        }
+        else if (valasztas == 3){
+            if (gepValasztas == 1){
+                vereseg();
+            }
+            else{
+                gyozelem();
+            }
+        }
+
     }
 
     private void init(){
